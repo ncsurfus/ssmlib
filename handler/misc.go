1ccc256ceebe0b582c228f7bda399b2c6b7391a4
ncsurfus <nathan.surfus@outlook.com>
2025-05-26 22:52:17 +1000

    Improve read/write cancellation

--- handler/misc.go
+++ handler/misc.go
@@ -18,7 +18,32 @@ var (
 	ErrWriteSizeMessage    = errors.New("failed to write size message to session")
 )
 
+type readResult struct {
+	n   int
+	err error
+}
+
+// If the context is cancelled, we can't reliably abort a message to the reader/writer. This is problematic
+// in some scenarios, like stdin likes to hang until there's input (even when closed!) These read/write data
+// in a separate goroutine through a channel. Technically, if the caller wanted to reuse this reader/writer
+// after stopping a handler, then our goroutine could "discard" a final reader/write.... this is an edge case
+// as everyone should mostly close their reader/writer, enabling the goroutine to cleanly exit.
+
 func CopySessionReaderToWriter(ctx context.Context, writer io.Writer, reader SessionReader) error {
+	writeErrC := make(chan error, 1)
+	writeC := make(chan []byte)
+	defer close(writeC)
+	go func() {
+		for {
+			payload, ok := <-writeC
+			if !ok {
+				return
+			}
+			_, err := writer.Write(payload)
+			writeErrC <- err
+		}
+	}()
+
 	for {
 		message, err := reader.Read(ctx)
 		if err != nil {
@@ -29,21 +54,56 @@ func CopySessionReaderToWriter(ctx context.Context, writer io.Writer, reader Ses
 			continue
 		}
 
-		_, err = writer.Write(message.Payload)
-		if err != nil {
-			return fmt.Errorf("%w: %w", ErrWriteData, err)
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case writeC <- message.Payload:
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case err = <-writeErrC:
+			if err != nil {
+				return fmt.Errorf("%w: %w", ErrWriteData, err)
+			}
 		}
 	}
 }
 
 func CopyReaderToSessionWriter(ctx context.Context, reader io.Reader, writer SessionWriter) error {
-	// In the future we should make this optionally buffered with a timer to flush data. This would
-	// enable callers to pick Latency vs Throughput. A small delay has been added to help prevent
-	// enable some buffering and prevent sending lots of tiny packets.
+	readResultC := make(chan readResult, 1)
+	readC := make(chan []byte)
+	defer close(readC)
+	go func() {
+		for {
+			buffer, ok := <-readC
+			if !ok {
+				return
+			}
+			n, err := reader.Read(buffer)
+			readResultC <- readResult{n: n, err: err}
+		}
+	}()
+
 	for {
 		// callers to read should always process any bytes first *and then* handle the error.)
 		buffer := make([]byte, 1024)
-		bytes, readerErr := reader.Read(buffer)
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case readC <- buffer:
+		}
+
+		var bytes int
+		var readerErr error
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case r := <-readResultC:
+			bytes, readerErr = r.n, r.err
+		}
+
 		if bytes > 0 {
 			message := messages.NewStreamMessage(buffer[:bytes])
 			writerErr := writer.Write(ctx, message)
